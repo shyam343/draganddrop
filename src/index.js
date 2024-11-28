89d7f2ababd14d3634449fb65aaebc942104ac25
Shyam Kumar Sah <shyam981628@gmail.com>
2024-11-28 18:49:17 +0545

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import $ from "jquery";
+import React, { Component, createRef } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.jQuery = $;
+window.$ = $;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+require("jquery-ui-sortable");
+require("formBuilder");
+
+const formData = [
+  {
+    type: "header",
+    subtype: "h1",
+    label: "formBuilder in React"
+  },
+  {
+    type: "paragraph",
+    label: "This is a demonstration of formBuilder running in a React project."
+  }
+];
+
+
+class FormBuilder extends Component {
+  fb = createRef();
+  componentDidMount() {
+    $(this.fb.current).formBuilder({ formData });
+  }
+
+  render() {
+    return (
+      <div className="bg-gray-100 p-4">
+      <div className="bg-white p-4 rounded-md shadow-md">
+        <div className="alert alert-info mb-4">
+          This is an example of how to use formBuilder with React. The JSON data for this form was set programmatically.
+        </div>
+        <div id="fb-editor" ref={this.fb} />
+      </div>
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<FormBuilder />, document.getElementById("root"));
